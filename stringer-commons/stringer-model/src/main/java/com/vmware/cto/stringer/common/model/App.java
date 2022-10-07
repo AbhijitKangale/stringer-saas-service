5b413502417398e7f7e2654565b1548292bc4da2
Abhijit Kangale <akangale@akangale-a01.vmware.com>
2022-10-07 15:33:40 +0530

    Added stringer repo structure

---
+++ stringer-commons/stringer-model/src/main/java/com/vmware/cto/stringer/common/model/App.java
@@ -0,0 +1,11 @@
+package com.vmware.cto.stringer.common.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ stringer-commons/stringer-utils/src/main/java/com/vmware/cto/stringer/common/utils/App.java
@@ -0,0 +1,11 @@
+package com.vmware.cto.stringer.common.utils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
